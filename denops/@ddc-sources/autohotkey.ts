fb7d2cea4270e8d88779e5657eff854b664460be
hokorobi <hokorobi.hokorobi@gmail.com>
2023-11-11 19:55:42 +0900

    Init

---
+++ denops/@ddc-sources/autohotkey.ts
@@ -0,0 +1,32 @@
+import {
+  BaseSource,
+  Item,
+} from "https://deno.land/x/ddc_vim@v4.1.0/types.ts";
+import {
+  Denops,
+} from "https://deno.land/x/ddc_vim@v4.1.0/deps.ts";
+
+type Params = Record<never, never>;
+
+export class Source extends BaseSource<Params> {
+  _cache: Item[] = [];
+
+  override async onInit(args: {
+    denops: Denops,
+  }): Promise<void> {
+    const data = Deno.readFileSync(new URL("../../rc/ahk2.standard.api", import.meta.url));
+    const lines = new TextDecoder().decode(data).split(/\r?\n/);
+    this._cache = [...new Set(lines)]
+      .filter((line) => line.length != 0)
+      .filter((word) => !word.startsWith(";"))
+      .map((word: string) => ({ word }));
+  }
+
+  override gather(): Promise<Item[]> {
+    return Promise.resolve(this._cache);
+  }
+
+  override params(): Params {
+    return {};
+  }
+}
